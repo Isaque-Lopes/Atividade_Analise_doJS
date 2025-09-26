12cc73d98b4817b1804252a7f1dd68bfda5a44a6
Isaque-Lopes <isaqueagosto31@gmail.com>
2025-09-26 10:27:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -5,15 +5,12 @@ let botao = document.getElementById('calcular');
 let res = document.getElementById('resultado');
 
 botao.addEventListener('click', function() {
-    // Get the input values and convert them to numbers
     let n1 = parseFloat(numero1.value);
     let n2 = parseFloat(numero2.value);
 
-    // Check if both inputs are valid numbers
     if (isNaN(n1) || isNaN(n2)) {
-        res.innerHTML = 'Por favor, insira números válidos!';
+        res.innerHTML = 'Números inválidos!';
     } else {
-        // Call the calcular function and display the result
         res.innerHTML = 'Resultado: ' + calcular(n1, n2);
     }
 });
